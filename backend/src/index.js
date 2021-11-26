8f69d2ba17e2b1b6f46a598a3f7707cb8d6005b1
Eduardo Brugnago <74428600+EduardoBrugnago@users.noreply.github.com>
2021-11-26 17:26:50 -0300

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,33 +10,3 @@ app.use(express.json());
 app.use(routes);
 
 app.listen(3333);
-
-/*
-METODOS HTTP:
-
-    GET: Buscar uma informacao no back-end (Buscar\Listar)
-    POST: Criar uma Informacao no back-end
-    PUT: Alterar uma informacao no back-end
-    DELETE: Deletar uma informacao no back-end
-*/
-
-/*
-TIPOS DE PARAMETRO:
-
-    QUERY PARAMS: Parametros nomeados enviados na rota apos o simbolo '?' (FILTROS, PAGINACAO)
-    ROUTE PARAMS: Parametro para identificar recursos
-    REQUEST BODY: Corpo da requisicao usado para criar ou alterar um recurso
-
-*/
-
-/*
-TIPOS DE BANCO DE DADOS:
-
-    SQL(Bancos Relacionais):
-    noSQL(Bancos Nao Relacionais):
-
-    --------------------------------
-
-    QUERY BUILDER: table('users').select('*').where()
-    
-*/
